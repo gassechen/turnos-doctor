84f3cf7929168af019322f0be77c5345e3d258b5
gassechen <noreply>
2014-10-25 17:05:35 +0000

    Add component agenda_doctores

---
+++ ui/agenda_doctores.reel/agenda_doctores.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/agenda_doctores.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Agenda_doctores
+ * @extends Component
+ */
+exports.Agenda_doctores = Component.specialize(/** @lends Agenda_doctores# */ {
+    constructor: {
+        value: function Agenda_doctores() {
+            this.super();
+        }
+    }
+});
