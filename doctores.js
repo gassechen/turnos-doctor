b3ce01a48426c9621ee9eeb2a528a23314fb7e68
gassechen <noreply>
2014-10-25 17:05:09 +0000

    Add module doctores

---
+++ doctores.js
@@ -0,0 +1,16 @@
+/**
+ * @module //doctores
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Doctores
+ * @extends Montage
+ */
+exports.Doctores = Montage.specialize(/** @lends Doctores# */ {
+    constructor: {
+        value: function Doctores() {
+            this.super();
+        }
+    }
+});
